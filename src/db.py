854e7c6445dbe9fe95ba7044346775e69d164932
yalexaner <mail.yalexaner@gmail.com>
2020-09-28 21:25:21 +0500

    creating Users class for db access

---
+++ src/db.py
@@ -0,0 +1,22 @@
+import pymysql
+
+con = pymysql.connect('localhost', 'root',
+                      '', 'enzbotdb')
+
+cur = con.cursor()
+
+
+class Users:
+    @staticmethod
+    def contains(id):
+        cur.execute(str.format('SELECT * FROM users WHERE UserId={0}', id))
+        return cur.fetchone()
+
+    @staticmethod
+    def create_new(id):
+        command = str.format("INSERT INTO users UserId VALUES ('{0}')", id)
+        cur.execute(command)
+
+    @staticmethod
+    def update_full_name(id, full_name):
+        pass
