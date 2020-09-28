12dfe39b27b401b9fd9cd063fa6385e6e4bde2b8
yalexaner <mail.yalexaner@gmail.com>
2020-09-28 21:30:31 +0500

    assigning specific logic to its classes

--- src/main.py
+++ src/main.py
@@ -3,7 +3,11 @@ import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.utils import get_random_id
-import pymysql
+
+from src.db import Users
+from src.user import User
+
+user = User()
 
 API_TOKEN = "***REMOVED***"
 API_VERSION = "5.131"
@@ -11,10 +15,6 @@ API_VERSION = "5.131"
 vk_session = vk_api.VkApi(token=API_TOKEN, api_version=API_VERSION)
 api = vk_session.get_api()
 longpoll = VkBotLongPoll(vk_session, group_id='184750146')
-
-con = pymysql.connect('localhost', 'root',
-                      '', 'enzbotdb')
-cur = con.cursor()
 
 
 # добавление пользователя в базу данных
@@ -39,29 +39,14 @@ def send_greetings(event):
                       random_id=get_random_id())
 
 
-def get_user(user_id):
-    cur.execute(str.format('SELECT * FROM users WHERE UserId={0}', user_id))
-    return cur.fetchone()
-
-
-def add_user(user_id):
-    user = api.users.get(user_ids=user_id)[0]
-    if user is None:
-        return
-    full_name = user['first_name'] + " " + user['last_name']
-    command = str.format("INSERT INTO users (UserId, FullName) VALUES ('{0}','{1}')",
-                           user_id, full_name)
-    cur.execute(command)
-
-
 def main():
     for event in longpoll.listen():
         if event.type == VkBotEventType.MESSAGE_NEW:
-            if get_user(event.message.from_id) is None:
+            if not Users.contains(event.message.from_id):
                 send_greetings(event)
-                add_user(event.message.from_id)
 
-            # проверить на наличие пользователя в базе данных. Занести при нажатии старт.
+            # запоминает id пользователя; если его нет в базе, добавляется
+            user.id = event.message.from_id
 
 
 def create_general_keyboard():
