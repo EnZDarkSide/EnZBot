82a23f7dd2118d402040e5c11c48540bc3687b16
EnZDarkSide <vladyavorsky478@gmail.com>
2020-10-11 01:34:03 +0500

    Rework of db functions

--- src/db.py
+++ src/db.py
@@ -1,26 +1,57 @@
 import pymysql
 
-con = pymysql.connect('sql7.freesqldatabase.com', 'sql7369933',
-                      'xWUCnPAEmW', 'sql7369933')
+from src.connection import get_global_con
+
+# con = get_local_con()
+con = get_global_con()
 
 cur = con.cursor()
 
 
 class Users:
+
     @staticmethod
-    def contains(id):
-        cur.execute(str.format('SELECT * FROM users WHERE UserId={0}', id))
+    def get_all():
+        Users.exec_command('SELECT * FROM users', False)
+        return cur.fetchall()
+
+    @staticmethod
+    def contains(user_id):
+        Users.exec_command(f'SELECT * FROM users WHERE UserId={user_id}', False)
         return cur.fetchone()
 
     @staticmethod
-    def create_new(id):
-        command = str.format("INSERT INTO users UserId VALUES ('{0}')", id)
-        cur.execute(command)
+    def add(user_id):
+        command = f"INSERT INTO users(UserId) VALUES ({user_id})"
+        return Users.exec_command(command)
 
     @staticmethod
-    def update_full_name(id, full_name):
-        pass
+    def add(user_id, full_name):
+        command = f"INSERT INTO users(UserId, FullName) VALUES ({user_id}, '{full_name}')"
+        return Users.exec_command(command)
+
+    # @staticmethod
+    # def update_full_name(user_id, full_name):
+    #     command = f"UPDATE users SET FullName='{full_name}' WHERE Id='{user_id}'"
+    #     return Users.exec_command(command)
 
     @staticmethod
-    def update_dorm(id, new_dom):
-        pass
+    def update_address(user_id, address):
+        command = f"INSERT INTO addresses (UserId, Address) VALUES({user_id},'{address}') ON DUPLICATE KEY UPDATE" \
+                  f" Address='{address}'"
+        return Users.exec_command(command)
+
+    @staticmethod
+    def exec_command(command, to_commit=True):
+        try:
+            affected_rows = cur.execute(command)
+
+            if to_commit:
+                con.commit()
+
+            print(f'affected rows: {affected_rows}')
+            print(f'command: {command}')
+            return True
+        except Exception as e:
+            print(e)
+            return False
