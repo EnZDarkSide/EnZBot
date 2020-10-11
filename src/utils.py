785c8fabdde41dd36b43ffc56a92315723634351
EnZDarkSide <vladyavorsky478@gmail.com>
2020-10-11 16:45:10 +0500

    one_time = True для клавиатуры

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,14 @@
-from typing import Tuple, List
-
+from typing import List
 from vkbottle import keyboard_gen
-from vkbottle.keyboard import Keyboard, Text
+
+
+def general_menu():
+    return create_keyboard([{"text": "Добавить группу"}, {"text": 'Указать адрес'}], [{"text": "Где трамваи?"}])
+
+
+def trolleys_menu():
+    return create_keyboard([{"text": 'Обновить данные'}, {"text": 'Указать адрес'}],
+                           [{"text": 'Главное меню', "color": "secondary"}])
 
 
 def create_keyboard(*rows: List[dict]):
@@ -11,6 +18,7 @@ def create_keyboard(*rows: List[dict]):
               "color": button["color"] if "color" in button else "primary",
               "type": button["type"] if "type" in button else "text"} for button in row]
             for row in rows
-        ]
+        ],
+        one_time=True
     )
     return keyboard
