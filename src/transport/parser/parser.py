c5034feba655b32c5f1934787fda4c0e3ac3d6df
yalexaner <mail.yalexaner@gmail.com>
2020-10-22 21:37:41 +0500

    creating function that returns stops with directions

--- src/transport/parser/parser.py
+++ src/transport/parser/parser.py
@@ -1,5 +1,5 @@
 import re
-from typing import List, Set
+from typing import Set, Tuple
 
 import requests
 from lxml import html
@@ -11,7 +11,7 @@ class Parser:
     def __init__(self):
         self.base_url = 'https://online.ettu.ru'
 
-    def get_trams(self, stop_id: int) -> List[Tram]:
+    def get_trams(self, stop_id: int) -> Tuple[Tram]:
         page = requests.get(f'{self.base_url}/station/{stop_id}')
         tree = html.fromstring(page.text)
 
@@ -19,11 +19,12 @@ class Parser:
 
         # проходится по всем элементам трамвайя — его номер, время прибытия и расстояние до него —,
         # берёт их текст и создаёт объект Tram для каждого из них
-        trams = [Tram(*map(lambda el: el.text_content(), tram_element)) for tram_element in tram_elements]
+        trams = tuple(Tram(*map(lambda el: el.text_content(), tram_element)) for tram_element in tram_elements)
 
         return trams
 
-    def get_stops(self, first_letter: str) -> Set[str]:
+    # возвращает названия остановок с их направлением
+    def get_raw_stops(self, first_letter: str) -> Tuple[str]:
         page = requests.get(f'{self.base_url}/stations/{first_letter.upper()}')
         tree = html.fromstring(page.text)
 
@@ -32,6 +33,14 @@ class Parser:
         # иначе берутся все ссылки, которые ниже заголовка 'Трамваи'
         tram_stop_els = tree.xpath('//h3[2]/preceding-sibling::a[@href]') \
                         or tree.xpath("//h3[contains(text(), 'Трамваи')][1]/following-sibling::a")
+
+        tram_stops_with_directions = tuple(map(lambda el: str(el.text or ''), tram_stop_els))
+
+        return tram_stops_with_directions
+
+    # возвращает названия остановок
+    def get_stops(self, first_letter: str) -> Set[str]:
+        tram_stop_els = self.get_raw_stops(first_letter)
 
         # ^((?:\w+ ?)+) \(.*$
         tram_stop_names = set(map(lambda el: re.sub(r'^([^(]+) \(.*$', r'\1', el.text), tram_stop_els))
