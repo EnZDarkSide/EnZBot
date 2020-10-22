f2f77e442c946689621b92aa26e6951fe231f5db
yalexaner <mail.yalexaner@gmail.com>
2020-10-22 21:40:32 +0500

    creating 'get_directions' function

--- src/transport/transport.py
+++ src/transport/transport.py
@@ -1,4 +1,5 @@
-from typing import List
+import re
+from typing import List, Iterator
 
 from .parser import Parser
 from .tram import Tram
@@ -16,8 +17,20 @@ class Transport:
         return stop in stops
 
     @staticmethod
-    def get_all_trams(stop_id: int) -> List[Tram]:
+    def get_trams(stop_id: int) -> Iterator[Tram]:
         trams = parser.get_trams(stop_id)
 
         # нужны только трамваи с номерами 14, 25 и 27
-        return list(filter(lambda tram: tram.number in (14, 25, 27), trams))
+        return filter(lambda tram: tram.number in (14, 25, 27), trams)
+
+    @staticmethod
+    def get_directions(tram_stop: str) -> List[str]:
+        tram_stop = tram_stop.lower()
+
+        directions = []
+        for tram_stop_with_direction in parser.get_raw_stops(tram_stop[0]):
+            if direction_match := re.match(rf'^{tram_stop} \(([^)]+)\)$', tram_stop_with_direction,
+                                           flags=re.IGNORECASE):
+                directions.append(direction_match.group(1))
+
+        return directions
--- tests/test_transport.py
+++ tests/test_transport.py
@@ -9,3 +9,19 @@ class TestStopExists(TestCase):
 
     def test_existent_stop(self):
         self.assertTrue(Transport.stop_exists('Ельцина'))
+
+
+class TestGetDirections(TestCase):
+    def test_stop_has_no_directions(self):
+        self.assertListEqual(Transport.get_directions('40 лет ВЛКСМ'), [])
+
+    def test_stop_has_two_directions(self):
+        self.assertListEqual(Transport.get_directions('1-й км'), ['на Пионерскую', 'на Техучилище'])
+
+    def test_stop_has_four_directions(self):
+        self.assertListEqual(Transport.get_directions('Блюхера'), [
+            'на ст.Шарташ',
+            'с Мира на Уральский федеральный университет',
+            'с Советской на Уральский федеральный университет',
+            'на Пионерскую'
+        ])
