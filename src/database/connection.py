0d89ffbb40f720cd7bd29a583c94fbcf92c14b2a
EnZDarkSide <vladyavorsky478@gmail.com>
2020-10-28 00:04:20 +0500

    Refactor

--- src/database/connection.py
+++ src/database/connection.py
@@ -10,7 +10,7 @@ def get_local_con():
 
 
 def get_global_con():
-    with open('../../settings.json') as connections_file:
+    with open('settings.json') as connections_file:
         connection = json.load(connections_file)
     return pymysql.connect(host=connection['host'], user=connection['login'], password=connection['pass'],
                            db=connection['database'], charset='cp1251')
