e0408d4ae9a1f1081d60846a71906dcf083af0e4
EnZDarkSide <vladyavorsky478@gmail.com>
2020-11-06 08:45:55 +0500

    portal refactoring

--- src/portal/utils.py
+++ src/portal/utils.py
@@ -1,9 +1,4 @@
 from expiringdict import ExpiringDict
-from vkbottle import Message
-from vkbottle.branch import Branch
-
-from src.database.enitities.Portal import DBPortal
-from src.portal.parser import PortalManager
 from src.utils import create_keyboard
 
 portal_users = ExpiringDict(max_len=10000, max_age_seconds=300)
