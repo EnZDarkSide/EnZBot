1f1f432fe84ac32d574fc912f486fb4e43775fe1
EnZDarkSide <vladyavorsky478@gmail.com>
2020-11-12 21:31:11 +0500

    ask group fix

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from vkbottle import Message
+from vkbottle.branch import Branch
 
 from src import messages, utils
 from src.bot import bot
+from src.database.enitities import DBGroups
 from src.utils import general_keyboard
 
 # приветствие
@@ -17,11 +19,19 @@ from src.utils import general_keyboard
 
 @bot.on.message(text=['Меню', 'Главное меню', '?'])
 async def send_menu(answer: Message):
+    if not DBGroups.get(answer.from_id):
+        await answer('Чтобы продолжить, вам нужно вписать группу для расписания')
+        return Branch('groups_update')
+
     await answer(message=messages.resp_show_menu, keyboard=utils.general_keyboard())
 
 
 @bot.on.message()
 async def rand_message(answer: Message):
+    if not DBGroups.get(answer.from_id):
+        await answer('Чтобы продолжить, вам нужно вписать группу для расписания')
+        return Branch('groups_update')
+
     await answer("Вызываем меню", keyboard=general_keyboard())
 
 
