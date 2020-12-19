7988569cb39b04a096c118de8df674743fe89fdb
yalexaner <mail.yalexaner@gmail.com>
2020-12-19 13:41:52 +0500

    editing tests for parser.get_stops function

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,44 +1,23 @@
 from unittest import TestCase
 
+from src.transport.entities.stop import Stop
 from src.transport.parser import TramParser
 
 
 class TestGetStops(TestCase):
-    def setUp(self):
-        self.parser = TramParser()
 
-    def test_one_stop_only(self):
-        self.assertSetEqual(self.parser.get_stops('Е'), {'Ельцина'})
+    def test_no_stops_available(self):
+        self.assertListEqual(TramParser.get_stops('2'), [])
 
-    def test_stop_name_contains_numbers(self):
-        self.assertSetEqual(self.parser.get_stops('1'), {'1-й км'})
+    def test_regular_case(self):
+        self.assertListEqual(TramParser.get_stops('1'), [
+            Stop(1168, '1-й км', 'на Пионерскую'),
+            Stop(1169, '1-й км', 'на Техучилище'),
+        ])
 
-    def test_stop_name_with_more_than_one_word(self):
-        self.assertSetEqual(self.parser.get_stops('А'),
-                            {'Автовокзал', 'Автомагистральная', 'Академия при Президенте России'})
-
-    def test_stop_name_contains_non_letter_characters(self):
-        self.assertSetEqual(self.parser.get_stops('Ю'), {'Ювелирная', 'Юго-Западная', 'Южная'})
-
-    def test_stop_on_page_with_no_trolley_header(self):
-        self.assertSetEqual(self.parser.get_stops('7'), {'7 Ключей'})
-
-    def test_stop_on_page_with_no_trams_on_it(self):
-        self.assertSetEqual(self.parser.get_stops('Я'), set())
-
-    def test_stop_names_with_no_parenthesis(self):
-        stop_names = {
-            'Ватутина',
-            'Верх-Исетский б-р',
-            'Верх-Исетский рынок',
-            'Вечный Огонь',
-            'ВИЗ',
-            'Викулова',
-            'Войкова',
-            'Волгоградская',
-            'Ворошиловская',
-            'Восточная',
-            'Вторчермет'
-        }
-
-        self.assertSetEqual(self.parser.get_stops('В'), stop_names)
+    def test_stop_has_no_direction(self):
+        self.assertListEqual(TramParser.get_stops('4'), [
+            Stop(3497, '40 лет ВЛКСМ', None),
+            Stop(1170, '40 лет Октября', 'на Диагностический центр'),
+            Stop(1171, '40 лет Октября', 'на ст. Машиностроителей')
+        ])
